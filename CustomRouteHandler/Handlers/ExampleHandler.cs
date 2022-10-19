30317bad2d3acf10301a627cd356bea72b7ed89a
tahaozturkk <ozturktaha1453@gmail.com>
2022-10-19 16:57:03 +0300

    first commit

---
+++ CustomRouteHandler/Handlers/ExampleHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CustomRouteHandler.Handlers
+{
+    public class ExampleHandler
+    {
+        public RequestDelegate Handler()
+        {
+            return async c =>
+            {
+               await c.Response.WriteAsync("Trial For Handler");
+
+            };
+        }
+      
+    }
+}
